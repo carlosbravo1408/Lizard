5e7ec4dd38f78598d4cee91942a30f8151c0c38f
carlos <carlos.j.bravo@hotmail.com>
2025-08-21 00:41:49 -0500

    feat: Add main.py script with user_name, github_token, and optional lizard_color (HEX) arguments to generate lizards for both light and dark themes
    A new main script, `main.py`, has been added to the project. This
    script is designed to be executed from the command line and accepts
    the following arguments: `user_name` (required), `github_token` (required),
    and an optional `lizard_color` in HEX format.
    
    This script will use the provided inputs to generate two versions of
    the lizard-contribution map SVG: one optimized for a dark theme and
    another for a light theme. This ensures that the generated visual
    representation is compatible and looks good in different viewing
    environments, providing greater flexibility and usability.

---
+++ main.py
@@ -0,0 +1,20 @@
+import sys
+
+from drawer.svg import ContributionGameSVG
+
+
+def main():
+    user_name = sys.argv[1]
+    token = sys.argv[2]
+    lizard_color = sys.argv[3] if len(sys.argv) > 3 else None
+
+    for theme in ["light", "dark"]:
+        ContributionGameSVG(
+            user_name,
+            token,
+            theme,
+            lizard_color=lizard_color
+        ).run()
+
+if __name__ == "__main__":
+    main()
